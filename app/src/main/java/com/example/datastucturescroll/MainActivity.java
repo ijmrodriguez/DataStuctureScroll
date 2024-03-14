427cccfbe51de67c8ad137a77801cf84fbc829d1
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-03-14 13:13:34 -0700

    declared variables

--- app/src/main/java/com/example/datastucturescroll/MainActivity.java
+++ app/src/main/java/com/example/datastucturescroll/MainActivity.java
@@ -10,10 +10,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    private QueueLinkedList queue;
+
+    private EditText nameEditText;
+    private EditText ageEditText;
+    private EditText gpaEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = new QueueLinkedList();
+        nameEditText = findViewById(R.id.nameEditText);
+        ageEditText = findViewById(R.id.ageEditText);
+        gpaEditText = findViewById(R.id.gpaEditText);
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener()
+        {
+
+        }
     }
 }
